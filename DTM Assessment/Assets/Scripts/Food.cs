1608267e5fa57a3d7d0c2bd4ca5ba95f655cccbb
Nathan0674 <101610894+Nathan0674@users.noreply.github.com>
2022-05-04 11:10:36 +1200

    Working Food Despawning
    Completely changed the method used to despawn food from manually comparing the distance between the player and food to using the Vector.Distance method. This is far more compact and simple and works far better.
    
    Food now despawns properly when out of a 20 unit radius of the player.

--- DTM Assessment/Assets/Scripts/Food.cs
+++ DTM Assessment/Assets/Scripts/Food.cs
@@ -7,12 +7,18 @@ public class Food : MonoBehaviour
     private Vector2 playerPos;
     private GameObject spawnManager;
     private SpawnManager spawnManagerScript;
+    public float DespawnDistance = 20.0f;
+    public float foodDistance;
     
     
 
     // Start is called before the first frame update
     void Start()
     {
+        // Allows me to access the "currentFood" variable:
+        // Calls the SpawnManager Gameobject and grabs the SpawnManager script component.
+        // Doing this lets me use the currentFood variable in a different script.
+        // This is needed so that the food count can update correctly when the instantiated food prefabs despawn.
         spawnManager = GameObject.Find("SpawnManager");
         spawnManagerScript = spawnManager.GetComponent<SpawnManager>();
     }
@@ -20,15 +26,23 @@ public class Food : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Gets the player position as a Vector2 and asigns this value to the "playerPos" vector variable.
         playerPos = GameObject.Find("Player").transform.position;
 
-        if (Mathf.Abs(transform.position.y) >= playerPos.y + 15 || Mathf.Abs(transform.position.x) >= playerPos.x + 15)
+        // Gets the distance between the "Player" Object and individual "Food" prefabs using the Vector2.Distance method.
+        // Assigns this value to the foodDistance variable.
+        foodDistance = Vector2.Distance(playerPos, transform.position);
+
+        // This if statement destroys food prefabs when they are out of a certain radius of the player.
+        // Its condition is that if the distance between the "Food" prefab and the "Player" GameObject is greater than the fixed value of the "despawnDistance" float:
+        // Destroy the "Food" prefab with the Destroy method. gameobject with a lower case "g" taregets itself.
+        // The script then references the currentFood variable from the SpawnManager script and uses this to update the currentFood by -1.
+        // This Means that the player can safely move away from an area with food without worrying about losing the food permanantly, as it will despawn and update the current food accordingly.
+        if (foodDistance > DespawnDistance)
         {
             Destroy(gameObject); 
-            spawnManagerScript.currentFood -= 1;       
+            spawnManagerScript.currentFood -= 1;
         }
-
-
     }
 
 }
